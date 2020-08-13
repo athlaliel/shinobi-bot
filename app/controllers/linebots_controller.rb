edbc1f4b0d085ea594753f8bfe6a306e43dd7493
athlaliel <athla8452@gmail.com>
2020-08-13 18:15:56 +0900

    initial commit

--- app/controllers/linebots_controller.rb
+++ app/controllers/linebots_controller.rb
@@ -12,18 +12,16 @@ class LinebotsController < ApplicationController
   end
 
   def send(line_ids, message)
-
     @post=Post.offset( rand(Post.count) ).first
-    # Postからランダムで返事を選ぶ。
+      # Postからランダムで返事を選ぶ。
       post('/v1/events', {
-            to: line_ids,
-            content: {
-                contentType: ContentType::TEXT,
-                toType: ToType::USER,
-                text: @post.name #返事をPostから取ってくる。
-            },
-            toChannel: TO_CHANNEL,
-            eventType: EVENT_TYPE,
-        })
-
+        to: line_ids,
+        content: {
+          contentType: ContentType::TEXT,
+          toType: ToType::USER,
+          text: @post.name #返事をPostから取ってくる。
+          },
+          toChannel: TO_CHANNEL,
+          eventType: EVENT_TYPE,
+      })
 end
