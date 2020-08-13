4920f4538447eac7716ab695a0faad30893d4c2d
athlaliel <athla8452@gmail.com>
2020-08-13 10:20:00 +0900

    initial commit

--- app/controllers/linebots_controller.rb
+++ app/controllers/linebots_controller.rb
@@ -6,8 +6,8 @@ class LinebotsController < ApplicationController
 
   def client
     @client ||= Line::Bot::Client.new { |config|
-      config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
-      config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
+      config.channel_secret = ENV['LINE_BOT_CHANNEL_SECRET']
+      config.channel_token = ENV['LINE_BOT_CHANNEL_TOKEN']
     }
   end
 
